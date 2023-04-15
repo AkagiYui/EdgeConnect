53305f6d313f8bd2515b5175e8a623941ce3cc2c
AkagiYui <akagiyui@yeah.net>
2023-04-16 00:52:44 +0800

    feat: create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# EdgeConnect 边际互联 服务端
+
+EdgeConnect Backend
---
+++ src/main/java/com/akagiyui/edgeconnect/EdgeConnectApplication.java
@@ -0,0 +1,16 @@
+package com.akagiyui.edgeconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author AkagiYui
+ */
+@SpringBootApplication
+public class EdgeConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeConnectApplication.class, args);
+    }
+
+}
