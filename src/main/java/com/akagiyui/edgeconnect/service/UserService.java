d4fa7de4fd5742f8d0db7480e3ca280ed2b3b745
AkagiYui <akagiyui@yeah.net>
2023-04-17 09:11:35 +0800

    feat: get user info from jdbc

--- src/main/java/com/akagiyui/edgeconnect/service/UserService.java
+++ src/main/java/com/akagiyui/edgeconnect/service/UserService.java
@@ -2,6 +2,8 @@ package com.akagiyui.edgeconnect.service;
 
 import com.akagiyui.edgeconnect.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
@@ -9,8 +11,8 @@ import java.util.List;
  * @author AkagiYui
  */
 
-public interface UserService extends IService<User> {
-
+public interface UserService extends IService<User>, UserDetailsService {
     List<User> getAllUser();
-
+    @Override
+    UserDetails loadUserByUsername(String username);
 }
--- src/main/java/com/akagiyui/edgeconnect/service/impl/UserServiceImpl.java
+++ src/main/java/com/akagiyui/edgeconnect/service/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.akagiyui.edgeconnect.service.impl;
 
+import com.akagiyui.edgeconnect.entity.LoginUserDetails;
 import com.akagiyui.edgeconnect.entity.User;
 import com.akagiyui.edgeconnect.mapper.UserMapper;
 import com.akagiyui.edgeconnect.service.UserService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.google.common.base.Preconditions;
+import jakarta.annotation.Resource;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +19,21 @@ import java.util.List;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+    @Resource
+    UserMapper userMapper;
+
     @Override
     public List<User> getAllUser() {
-        return getBaseMapper().selectList(null);
+        return userMapper.selectList(null);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(User::getUsername, username);
+        User user = userMapper.selectOne(wrapper);
+        Preconditions.checkNotNull(user, "用户不存在");
+        return new LoginUserDetails(user);
     }
 }
