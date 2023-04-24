e8405e1a56efb8d1ce0ac677b3e945265d078ae7
AkagiYui <akagiyui@yeah.net>
2023-04-24 19:25:10 +0800

    feat: add cors config

---
+++ src/main/java/com/akagiyui/edgeconnect/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.akagiyui.edgeconnect.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域配置类
+ * @author AkagiYui
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 允许跨域访问的路径
+        registry.addMapping("/**")
+                // 允许跨域访问的源
+                .allowedOriginPatterns("*")
+                // 允许请求方法
+                .allowedMethods("*")
+                // 预检间隔时间
+                .maxAge(168000)
+                // 允许头部设置
+                .allowedHeaders("*")
+                // 是否发送cookie
+                .allowCredentials(true);
+    }
+}
